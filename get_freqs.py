a15697a7e2e1cfabffd1cc711d45f681e4390ee6
Emilio <epelaaez@gmail.com>
2021-07-15 08:11:30 -0500

    remove extra variable

--- get_freqs.py
+++ get_freqs.py
@@ -1,8 +1,11 @@
+import numpy as np
+from scipy import fft
+
 def get_freqs(signal, dt, threshold):
     """
     Returns the most significant frequencies found in noisy signal.
     Using the data from create_data.py, the threshold needs to be low (~0.001) because of the amplitude factor.
-    For normal trigonometric functions, the threshold can be higher (~100).
+    For normal trigonometric functions, the threshold can be higher (~100), but still depends on the amplitude.
 
     Parameters:
     -----------
@@ -15,18 +18,17 @@ def get_freqs(signal, dt, threshold):
 
     Output:
     -------
-        frequencies: np.array
+        freqs: np.array
             Array with most significant frequencise found
     """
     n    = len(signal)
-    fhat = fft(signal)
-    psd  = fhat * np.conj(fhat) / n
-    freq = (1 / (dt * n)) * np.arange(n)
-    L    = np.arange(0, np.floor(n/2), dtype='int') # half of the domain since we know FT is symmetric
+    fhat = fft(signal)                               # get noisy fast fourier transform
+    psd  = fhat * np.conj(fhat) / n                  # get power spectrum density
+    L    = np.arange(0, np.floor(n/2), dtype='int')  # half of the domain since we know FT is symmetric
 
     freqs = []
     for i in L:
-        if psd[i] >= threshold:
+        if psd[i] >= threshold:                      # compare psd of each frequency against the threshold
             freqs.append(i)
 
     return np.array(freqs)
