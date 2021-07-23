ee34e29a43805616fe86709dd22fb6eb2e05aab1
Emilio <epelaaez@gmail.com>
2021-07-23 09:17:26 -0500

    edit chi2 testing

--- chi_squared_testing.py
+++ chi_squared_testing.py
@@ -3,12 +3,12 @@ from hff import get_hff
 from create_data import create_data
 from chi_squared import get_prob
 
-test_stds = np.arange(0.4, 0.5, 0.01)
-freq      = 5
+test_stds = np.arange(0.3, 0.4, 0.01)
+ex_freq   = 5
 
 for std in test_stds:
     print(f"Error deviation: {std}")
-    signal    = create_data(m_phi=np.pi * freq, deviation=std)
+    signal    = create_data(m_phi=np.pi * ex_freq, deviation=std)
     idx, freq = get_hff(np.array(signal))
     print(f'Estimated frequency: {freq}') 
     get_prob(signal, idx)  
