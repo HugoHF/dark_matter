7f4d48a4e2286d462341fac3fd27984823da303b
Emilio <epelaaez@gmail.com>
2021-07-22 23:07:36 -0500

    add comments to chi2

--- chi_squared.py
+++ chi_squared.py
@@ -3,16 +3,16 @@ from scipy.fft import fft
 from scipy.stats import chi2
 
 def get_prob(signal, idx):
-    transform  = fft(signal[1])
-    peak_value = transform[idx]
+    transform  = fft(signal[1]) # gets the FT of the values of the signal
+    peak_value = transform[idx] # gets the FT coefficient for the detected frequency found by HFF
 
     real_std  = np.std(np.real(transform))
     imag_std  = np.std(np.imag(transform))
-    avg_std   = (real_std + imag_std) / 2
+    avg_std   = (real_std + imag_std) / 2  # calculate deviation of real and imaginary part of the Ft and average them
 
     print(f"Real std: {real_std}. Imag std: {imag_std}")
 
     dof = 2
     x   = (np.abs(peak_value) ** 2) / (avg_std ** 2)
-    chi =  chi2.cdf(x, 2)
+    chi =  chi2.cdf(x, dof)
     print(f"Prob: {chi}")
