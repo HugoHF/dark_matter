0880056563c6596dc68dc5e18905510d8c801cfd
RogerPenrose <ogobogo5@gmail.com>
2021-07-25 18:50:22 +0200

    Takes freq too now

--- chisquared_stuff.py
+++ chisquared_stuff.py
@@ -3,12 +3,9 @@ from scipy.stats import chi2
 from scipy.special import binom
 from decimal import Decimal
 
-def get_significance(fourier):
+def get_significance(fourier, index_freq):
 
-    maximum = max(fourier)
-    index_max = list(fourier).index(maximum)
-
-    fourier_without_peak = np.delete(fourier, np.argwhere(fourier==maximum))
+    fourier_without_peak = np.delete(fourier, np.argwhere(fourier==fourier[freq]))
 
 
     real = np.real(fourier_without_peak)
@@ -19,10 +16,10 @@ def get_significance(fourier):
     fourier_conj = np.conj(fourier)
     fourier2 = [np.real(fourier[i]*fourier_conj[i]) for i in range(len(fourier))]
 
-    cdf_result = cdf_of_chi2(max(fourier2), std_total**2)
+    cdf_result = cdf_of_chi2(fourier2[freq], std_total**2)
 
 
-    actual_probability =  ((cdf_result)**(len(fourier2)))
+    actual_probability =  cdf_result**(len(fourier2))
     return actual_probability
 
 def cdf_of_chi2(peak_height, sdt_x):
