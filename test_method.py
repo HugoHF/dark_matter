59fb9aa148061a5f4240e8192da76bf452250894
RogerPenrose <ogobogo5@gmail.com>
2021-07-26 22:58:48 +0200

    final visualization tool

--- test_method.py
+++ test_method.py
@@ -1,9 +1,15 @@
 import json
+
 from main import methods as func # PUT IN HERE THE CORRECT METHOD
+
+from mpl_toolkits import mplot3d
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
 np.seterr(all="ignore")
+
+X, Y = [], []
 
 with open('test_samples.json', "r") as file:
     testing_samples = json.load(file)
@@ -29,31 +35,62 @@ for freq in signals.keys():
 freq_mat = np.array([np.array([results["freqs"][frequency][deviation] for deviation in results["freqs"][frequency].keys()]) for frequency in results["freqs"].keys()])
 error_mat = np.array([np.array([results["errors"][frequency][deviation] for deviation in results["errors"][frequency].keys()]) for frequency in results["errors"].keys()])
 
+X, Y = list(results["freqs"].keys()), list(results["freqs"]["0.0"].keys())
+X, Y = [float(i) for i in X], [float(i) for i in Y]
+# Make matrices
+X, Y = np.array([X for i in range(len(Y))]), np.array([Y for i in range(len(X))])
+X = X.T
 
-fig, ax = plt.subplots(1,2)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+fig.tight_layout()
+# X = np.tile(X, len(Y))
+# Y = np.tile(Y, len(X))
 
-ax[0].matshow(freq_mat, label="output frequencies")
-ax[0].set_xlabel("input frequencies")
-ax[0].set_ylabel("standard deviations")
+ax.plot_wireframe(X,Y,error_mat)
 
-ax[1].matshow(error_mat, label="significances")
-ax[1].set_xlabel("input frequencies")
-ax[1].set_ylabel("standard deviations")
-
-ax[0].legend()
-ax[1].legend()
-
-cmaps = ['RdBu_r', 'viridis']
-pcm = ax[0].pcolormesh(freq_mat, cmap=cmaps[0])
-fig.colorbar(pcm, ax=ax[0])
-pcm = ax[1].pcolormesh(freq_mat, cmap=cmaps[1])
-fig.colorbar(pcm, ax=ax[1])
-
-# for col in range(2):
-#     for row in range(2):
-#         ax = axs[row, col]
-
-
-fig.suptitle("results for method _")
-plt.tight_layout()
+ax.set_xlabel("original frequencies")
+ax.set_ylabel("original error deviations")
+ax.set_zlabel("calculated significance")
 plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+fig.tight_layout()
+# X = np.tile(X, len(Y))
+# Y = np.tile(Y, len(X))
+
+ax.plot_wireframe(X,Y,freq_mat)
+
+ax.set_xlabel("original frequencies")
+ax.set_ylabel("original error deviations")
+ax.set_zlabel("calculated frequencies")
+plt.show()
+
+# fig, ax = plt.subplots(1,2)
+#
+# ax[0].matshow(freq_mat, label="output frequencies")
+# ax[0].set_xlabel("input frequencies")
+# ax[0].set_ylabel("standard deviations")
+#
+# ax[1].matshow(error_mat, label="significances")
+# ax[1].set_xlabel("input frequencies")
+# ax[1].set_ylabel("standard deviations")
+#
+# ax[0].legend()
+# ax[1].legend()
+#
+# cmaps = ['RdBu_r', 'viridis']
+# pcm = ax[0].pcolormesh(freq_mat, cmap=cmaps[0])
+# fig.colorbar(pcm, ax=ax[0])
+# pcm = ax[1].pcolormesh(freq_mat, cmap=cmaps[1])
+# fig.colorbar(pcm, ax=ax[1])
+#
+# # for col in range(2):
+# #     for row in range(2):
+# #         ax = axs[row, col]
+#
+#
+# fig.suptitle("results for method _")
+# plt.tight_layout()
+# plt.show()
